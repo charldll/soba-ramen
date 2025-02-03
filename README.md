d811e88f85aae93e51feb695d2fe1f0ccf462d27
unknown <pawelecinga@gmail.com>
2025-02-03 19:25:00 +0100

    starter pack

---
+++ README.md
@@ -0,0 +1,7 @@
+first edition: 3/02/2025
+zainstalowane komponenty:
+    axios
+    react-routes
+    lucide
+    swr
+    validator.js
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">home</h1>
+    </>
+  )
+}
+
+export default App
