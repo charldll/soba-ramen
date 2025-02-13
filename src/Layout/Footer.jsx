e6badc7af75069b9ea2f9bd6551933f62a65774d
Zaklina <zaklinajasinska@gmail.com>
2025-02-14 00:23:20 +0100

    naprawiony odnośnik do sociali oraz poprawiona nawigacja

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,23 +1,32 @@
-import { Instagram, Facebook } from 'lucide-react';
-import { NavLink } from 'react-router';
+import { Instagram, Facebook } from "lucide-react";
+import { NavLink } from "react-router";
 
 export default function Footer() {
   return (
-    <div className="flex relative bottom-0 bg-[#5780DC]">
+    <div className="relative bottom-0 flex rounded-sm bg-[#5780DC]">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex p-1 items-center justify-between">
-        <div className="flex basic-1/5">
-          <NavLink to="https://www.instagram.com/">
-        <Instagram/> 
-        </NavLink>
-        <NavLink to="https://www.facebook.com/">
-        <Facebook/>
-        </NavLink>
-        </div>
-        <div className="flex">
-        <p> Created by: Agnieszka Wilczek, Inga Pawelec, Żaklina Jasińska</p>
-        <p>© 2025</p>
-        </div>
+        <div className="relative flex items-center justify-between p-1">
+          <div className="basic-1/5 flex">
+            <NavLink
+              to="https://www.instagram.com/soba.ramen1/"
+              target="_blank"
+            >
+              <Instagram />
+            </NavLink>
+            <NavLink
+              to="https://www.facebook.com/profile.php?id=61572853306851"
+              target="_blank"
+            >
+              <Facebook />
+            </NavLink>
+          </div>
+          <div className="flex">
+            <p>
+              {" "}
+              Created by: Agnieszka Wilczek, Inga Pawelec, Żaklina Jasińska
+            </p>
+            <p>© 2025</p>
+          </div>
         </div>
       </div>
     </div>
--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -7,7 +7,7 @@ import Logo from "./imgs/soba-logo.svg";
 
 const navigation = [
   { name: "Menu", href: "/menu" },
-  { name: "Ramen", href:"/custom-ramen" },
+  { name: "Ramen", href: "/custom-ramen" },
   { name: "Kontakt", href: "/contact" },
   { name: "Admin", href: "/admin" },
 ];
@@ -16,46 +16,51 @@ export default function Navbar() {
   const location = useLocation();
 
   return (
-    <Disclosure as="nav" className="bg-[#E14F52] rounded-sm">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 ">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="flex basis-1/5">
-            <Link to="/">
-              <img src={Logo} width={"90px"}/>
-            </Link>
-          </div>
-          <div className="flex sm:ml-6 sm:block basis-4/5">
-            <div className="flex max-sm:hidden space-x-4 justify-self-end">
-              {navigation.map((item) => (
-                  <AppNavLink
-                    key={item.name}
-                    to={item.href}
-                    location={location}
-                  >
-                    {item.name}
-                  </AppNavLink>
-                ))}
+    <Disclosure as="nav" className="rounded-sm bg-[#E14F52]">
+      {({ close }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="flex basis-1/5">
+                <Link to="/">
+                  <img src={Logo} width={"90px"} />
+                </Link>
+              </div>
+              <div className="flex basis-4/5 sm:ml-6 sm:block">
+                <div className="flex space-x-4 justify-self-end max-sm:hidden">
+                  {navigation.map((item) => (
+                    <AppNavLink
+                      key={item.name}
+                      to={item.href}
+                      location={location}
+                    >
+                      {item.name}
+                    </AppNavLink>
+                  ))}
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+                <AppMobileMenuButton />
+              </div>
             </div>
           </div>
-          <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
-            <AppMobileMenuButton />
-          </div>
-        </div>
-      </div>
 
-      <DisclosurePanel className="sm:hidden">
-        <div className="space-y-1 px-2 pt-2 pb-3">
-          {navigation.map((item) => (
-            <AppMobileMenuNavLink
-              key={item.name}
-              to={item.href}
-              location={location}
-            >
-              {item.name}
-            </AppMobileMenuNavLink>
-          ))}
-        </div>
-      </DisclosurePanel>
+          <DisclosurePanel className="sm:hidden">
+            <div className="space-y-1 px-2 pt-2 pb-3">
+              {navigation.map((item) => (
+                <AppMobileMenuNavLink
+                  key={item.name}
+                  to={item.href}
+                  location={location}
+                  onClick={() => close()}
+                >
+                  {item.name}
+                </AppMobileMenuNavLink>
+              ))}
+            </div>
+          </DisclosurePanel>
+        </>
+      )}
     </Disclosure>
   );
 }
