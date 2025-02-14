fff7e0d724b4df8f276d93a6688e34077c477d8c
veelcheck <veelcheck@gmail.com>
2025-02-14 20:23:58 +0100

    nav in KitchenDashboard

--- src/routes/KitchenDashboard.jsx
+++ src/routes/KitchenDashboard.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable no-unused-vars */
+import { useState } from "react";
 import { X, CookingPot } from "lucide-react";
+import { Link } from "react-router";
+
 import ButtonComponent from "../components/ButtonComponent";
 import useKitchen from "../hooks/useKitchen";
 import { useAuth } from "../context/AuthContext";
 import { getTableNumber } from "../utils/getTableNumber";
 import { getCurrentDate } from "../utils/getCurrentDate";
-import { useState } from "react";
+import Logo from "../Layout/imgs/soba-logo.svg";
 
 const KitchenDashboard = () => {
   const { orders, formatTime, deleteOrder, serveOrder } = useKitchen();
@@ -31,12 +34,13 @@ const KitchenDashboard = () => {
   };
 
   return (
-    <div className="bg-grid">
-      <main className="mx-auto max-w-7xl p-2">
-        <h1 className="mx-auto flex w-fit flex-col rounded-md px-6 py-3 text-center text-xl font-bold sm:flex-row">
-          ZAMÓWIENIA <span className="ml-4"> {getCurrentDate()}</span>
-        </h1>
-        <div className="flex items-center justify-center">
+    <div className="bg-grid min-h-screen">
+      <nav className="bg-menu-red">
+        <div className="mx-auto flex min-h-16 max-w-7xl flex-wrap items-center justify-between px-2 py-2 sm:px-6 lg:px-8">
+          <Link to="/">
+            <img src={Logo} width={"90px"} />
+          </Link>
+
           <ButtonComponent
             onClick={signOut}
             className="bg-logo-blue font-semibold text-white hover:bg-gray-700"
@@ -44,6 +48,15 @@ const KitchenDashboard = () => {
             Wyloguj
           </ButtonComponent>
         </div>
+      </nav>
+      <main className="mx-auto max-w-7xl p-2">
+        <h1 className="mx-auto flex w-fit flex-col rounded-md px-6 py-3 text-center text-xl font-bold sm:flex-row">
+          ZAMÓWIENIA <span className="ml-4"> {getCurrentDate()}</span>
+        </h1>
+
+        {orders.length === 0 && (
+          <h2 className="px-2 py-10 text-center text-3xl">Brak zamówień</h2>
+        )}
         <div className="grid grid-cols-[repeat(auto-fit,minmax(240px,1fr))] gap-2 px-2 py-10 lg:grid-cols-[repeat(auto-fit,minmax(400px,1fr))]">
           {orders.map((order) => (
             <section
