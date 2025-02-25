5eeba655352752fbb9404ee8970219e7132557b2
Zaklina <zaklinajasinska@gmail.com>
2025-02-25 10:06:28 +0100

    Component AboutTeam

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,9 +1,7 @@
 import BlurText from "../utils/animations/BlurText";
 import Image from "../Layout/imgs/about.webp";
-import Inga from "../Layout/imgs/Inga.png";
-import Agnieszka from "../Layout/imgs/Agnieszka.png";
-import Żaklina from "../Layout/imgs/Żaklina.png";
 import ScrollToTopButton from "../components/ScrollToTopButton";
+import AboutTeam from "../components/AboutTeam";
 
 export default function About() {
   return (
@@ -54,51 +52,7 @@ export default function About() {
             coś wyjątkowego do naszej ramenowni – od sztuki kulinarnej, przez
             kreatywność, aż po ciepłą, przyjazną atmosferę.
           </p>
-          <div className="mt-10 grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-            <div className="flex flex-col items-center text-center">
-              <img
-                src={Agnieszka}
-                alt="Agnieszka"
-                className="h-50 w-50 transform rounded-full object-cover transition duration-300 hover:scale-110"
-              />
-              <h2 className="text-menu-red mt-4 text-xl font-semibold">
-                Agnieszka
-              </h2>
-              <p className="mt-5 text-gray-700 md:text-justify">
-                Mistrz smaku i aromatu. Dba o perfekcyjnie ugotowane buliony,
-                sprężysty makaron i idealnie dobrane dodatki. Dzięki niej każda
-                miska ramenu to dzieło sztuki!
-              </p>
-            </div>
-            <div className="flex flex-col items-center text-center">
-              <img
-                src={Inga}
-                alt="Inga"
-                className="h-50 w-50 transform rounded-full object-cover transition duration-300 hover:scale-110"
-              />
-              <h2 className="text-menu-red mt-4 text-xl font-semibold">Inga</h2>
-              <p className="mt-5 text-gray-700 md:text-justify">
-                Osoba odpowiedzialna za atmosferę, design i doświadczenie.
-                Tworzy naszą historię, dba o wystrój i sprawia, że ramenownia to
-                nie tylko miejsce na jedzenie, ale i prawdziwa przygoda!
-              </p>
-            </div>
-            <div className="flex flex-col items-center text-center md:col-span-2 lg:col-span-1">
-              <img
-                src={Żaklina}
-                alt="Żaklina"
-                className="h-50 w-50 transform rounded-full object-cover transition duration-300 hover:scale-110"
-              />
-              <h2 className="text-menu-red mt-4 text-xl font-semibold">
-                Żaklina
-              </h2>
-              <p className="mt-5 text-gray-700 md:text-justify">
-                Serce naszej ramenowni. Dba o każdy szczegół, od najwyższej
-                jakości składników po niezapomnianą obsługę gości. Sprawia, że
-                każdy czuje się jak w domu.
-              </p>
-            </div>
-          </div>
+          <AboutTeam></AboutTeam>
           <p className="mt-20 text-center text-lg leading-relaxed text-gray-700 md:text-justify md:text-xl">
             <span className="text-menu-red font-semibold">
               Razem tworzymy coś więcej niż restaurację{" "}
