81e742aa08d451fe5557f1dcaa42eedfe6c6bc99
veelcheck <veelcheck@gmail.com>
2025-02-26 21:01:54 +0100

    AboutTeamCard mapped in About Section

---
+++ src/components/AboutTeamCard.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+const AboutTeamCard = ({ name, description, image }) => {
+  return (
+    <>
+      <div className="flex flex-col items-center text-center md:last:col-span-2 lg:last:col-span-1">
+        <img
+          src={image}
+          alt={`image of ${name}`}
+          className="h-50 w-50 transform rounded-full object-cover transition duration-300 hover:scale-110"
+        />
+        <h2 className="text-menu-red mt-4 text-xl font-semibold">{name}</h2>
+        <p className="mt-5 max-w-[26rem] text-gray-700 md:text-justify">
+          {description}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default AboutTeamCard;
